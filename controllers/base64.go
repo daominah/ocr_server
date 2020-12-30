716f5e6efa1d045b2187d89fd1bdbd19236755db
Đào Thanh Tùng <tung.dao@techx.vn>
2020-12-30 11:25:18 +0700

    add base64 handle jpeg

--- controllers/base64.go
+++ controllers/base64.go
@@ -1,13 +1,15 @@
 package controllers
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"image/jpeg"
+	"image/png"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"regexp"
 	"strings"
 
 	"github.com/mywrap/gofast"
@@ -48,11 +50,25 @@ func Base64(w http.ResponseWriter, r *http.Request) {
 		render.JSON(http.StatusBadRequest, fmt.Errorf("base64 string required"))
 		return
 	}
-	body.Base64 = regexp.MustCompile("data:image\\/png;base64,").ReplaceAllString(body.Base64, "")
+	isJpg := false
+	if strings.HasPrefix(body.Base64, `data:image/jpeg;base64,`) {
+		isJpg = true
+	}
+	body.Base64 = strings.TrimPrefix(body.Base64, `data:image/png;base64,`)
+	body.Base64 = strings.TrimPrefix(body.Base64, `data:image/jpeg;base64,`)
 	b, err := base64.StdEncoding.DecodeString(body.Base64)
 	if err != nil {
-		render.JSON(http.StatusBadRequest, err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf("error base64 DecodeString: %v", err)))
 		return
+	}
+	if isJpg {
+		b, err = ConvertJpgToPng(b)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(fmt.Sprintf("error ConvertJpgToPng: %v", err)))
+			return
+		}
 	}
 	tempfile.Write(b)
 
@@ -81,4 +97,24 @@ func Base64(w http.ResponseWriter, r *http.Request) {
 	log.Debugf("response of base64ed [%.1f kB, %v]: %v", float64(len(b))/1024,
 		body.Base64[:gofast.MinInts(32, len(body.Base64))], ret)
 	render.JSON(http.StatusOK, map[string]interface{}{"result": ret})
+}
+
+func ConvertJpgToPng(imageBytes []byte) ([]byte, error) {
+	imgType := http.DetectContentType(imageBytes)
+	switch imgType {
+	case "image/png":
+		return imageBytes, nil
+	case "image/jpeg":
+		img, err := jpeg.Decode(bytes.NewReader(imageBytes))
+		if err != nil {
+			return nil, fmt.Errorf("decode jpeg: %v", err)
+		}
+		buf := new(bytes.Buffer)
+		if err := png.Encode(buf, img); err != nil {
+			return nil, fmt.Errorf("encode png: %v", err)
+		}
+		return buf.Bytes(), nil
+	default:
+		return nil, fmt.Errorf("unsupported image type %v", imgType)
+	}
 }
